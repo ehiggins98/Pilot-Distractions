7a68f6222814bbe29c32c69af4fae42768c6fb6d
ehiggins98 <ericdhiggins@gmail.com>
2019-05-06 00:42:15 -0500

    Added trained model

--- dnn/parameter_tuning.py
+++ dnn/parameter_tuning.py
@@ -8,13 +8,15 @@ features = input.get_optimal_features()
 
 model = get_model(len(features), batch_size, [32, 64, 128, 256, 128, 64, 32, 16])
 model.fit(
-    x=input.get_dataset('../train.tfrecord', batch_size, features, input.match_extrema(features)),
+    x=input.get_dataset('train.tfrecord', batch_size, features, input.match_extrema(features)),
     epochs=1,
     shuffle=True,
     steps_per_epoch=int(4800000/batch_size)
 )
 
 model.evaluate(
-    x=input.get_dataset('../dev.tfrecord', batch_size, features, input.match_extrema(features)),
+    x=input.get_dataset('dev.tfrecord', batch_size, features, input.match_extrema(features)),
     steps=int(48000/batch_size)
 )
+
+model.save_weights('model.hdf5')
